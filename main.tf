013c5252fc352e52f230d4439c9c76204cf8454c
swaroop <swaroopaws2020@gmail.com>
2020-05-20 17:39:41 +0530

    my terraform code

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region     = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_vpc" "myvpc-test" {
+  cidr_block       = "${var.vpc_cidr}"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "${var.vpc_name}"
+    Env = "${var.vpc_env_name}"
+    Owner = "${var.owner_name}"
+    
+  }
+}
+  
+resource "aws_internet_gateway" "myigw-1" {
+    vpc_id = "${aws_vpc.myvpc-test.id}"
+
+    tags = {
+        Name = "${var.igw_name}"
+        Env = "${var.igw_env_name}"
+    }
+  
+}
+
+resource "aws_subnet" "mysubnet_public-1" {
+  vpc_id     = "${aws_vpc.myvpc-test.id}"
+  cidr_block = "${var.subnet_cidr-1}"
+  availability_zone = "${var.az1}"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "${var.subnet_name-1}"
+    Env = "${var.subnet_env}"
+  }
+}
+
+resource "aws_subnet" "mysubnet_public-2" {
+  vpc_id     = "${aws_vpc.myvpc-test.id}"
+  cidr_block = "${var.subnet_cidr-2}"
+  availability_zone = "${var.az2}"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "${var.subnet_name-2}"
+    Env = "${var.subnet_env}"
+  }
+}
+
+resource "aws_subnet" "mysubnet_public-3" {
+  vpc_id     = "${aws_vpc.myvpc-test.id}"
+  cidr_block = "${var.subnet_cidr-3}"
+  availability_zone = "${var.az3}"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "${var.subnet_name-3}"
+    Env = "${var.subnet_env}"
+  }
+}
+
+
+  
+
+
---
+++ var.tf
@@ -0,0 +1,33 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+
+variable "owner_name" {}
+
+variable "vpc_env_name" {}
+variable "igw_name" {}
+
+variable "igw_env_name" {}
+
+variable "subnet_cidr-1" {}
+variable "subnet_cidr-2" {}
+variable "subnet_cidr-3" {}
+variable "subnet_name-1" {}
+
+variable "subnet_name-2" {}
+variable "subnet_name-3" {}
+
+variable "subnet_env" {}
+variable "az1" {}
+variable "az2" {}
+variable "az3" {}
+
+
+
+
+
+
+
+
