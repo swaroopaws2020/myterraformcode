5de9b9ac03c8b34de7d72b01bf7c3a17c681b2ea
swaroop <swaroopaws2020@gmail.com>
2020-05-21 07:21:13 +0000

    Lock ID changed for dynamo db

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "myterraform-code_lock" {
   name             = "myterraform-code_lock"
-  hash_key         = "LockId"
+  hash_key         = "LockID"
   billing_mode     = "PAY_PER_REQUEST"
   
   attribute {
-    name = "LockId"
+    name = "LockID"
     type = "S"
   }
 }
